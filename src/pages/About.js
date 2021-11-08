3a5daaf35831ccf5b5818fcc7a03e22b189cc695
Pramod Suthar <pramodsuthar@mail.com>
2021-11-07 21:49:17 -0400

    final commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">About us</h1>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis labore, quos alias adipisci minima iste quod corporis dolorem, neque quibusdam sapiente cupiditate quas aspernatur delectus amet sed asperiores autem non facilis quisquam. Soluta beatae iste repellat est!
+            </p>
+        </section>
+    )
+}
+
+export default About
